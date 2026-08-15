a3126428bd494b0340e3f411bc8cf93cccca2baf
uykut38 <ummuadam0@gmail.com>
2026-08-15 22:44:24 +0200

    بىرىنجى تاپشۇرۇق

---
+++ 2.دەرس تاپشۇرۇق.py
@@ -0,0 +1,9 @@
+name = input(":ئىسمىڭىزنى كىرگۈزۈڭ")
+age = int(input("يېشىڭىزنى كىرگۈزۈڭ:"))
+height = float(input("بوي ئېگىزلىكىڭىزنى كىرگۈزۈڭ(cm):"))
+study_hours = float(input("بىر كۈندە قانچە سائەت ئوقۇيسىز؟"))
+
+print(name,"ئىسمىڭىز:")
+print("يېشىڭىز:",age)
+print("بوي ئېگىزلىكىڭىز",height,"cm")
+print("بىركۈندە ئوقۇش ۋاقتىڭىز",study_hours,"سائەت")
